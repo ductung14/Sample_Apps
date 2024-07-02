6ba2cf89f33f7b831df4dffaa15ff91ae1819051
ductung <nguyenductung224@gmail.com>
2024-07-02 14:39:57 +0700

    Chapter7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = I18n.available_locales.include?(locale) ?
       locale : I18n.default_locale
   end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
